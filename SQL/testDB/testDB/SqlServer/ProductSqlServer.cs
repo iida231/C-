edc034d737b8e67d70f5c614e104358c6026ea88
iida231 <zukuzuku0406@example.com>
2021-10-31 18:05:35 +0900

    Helperクラスの実装

--- SQL/testDB/testDB/SqlServer/ProductSqlServer.cs
+++ SQL/testDB/testDB/SqlServer/ProductSqlServer.cs
@@ -11,24 +11,9 @@ namespace testDB.SqlServer
 {
     public static class ProductSqlServer
     {
-        private static string _connectionString;
         static ProductSqlServer()
         {
-            //SQL接続にはDB名とテーブル名が必要である
-            //DB接続するためのコネクション
-            var builder = new SqlConnectionStringBuilder();
-
-            //サーバ名
-            //\があるとエラーになるため＠をつける
-            builder.DataSource = @"LAPTOP-CTHD7A6U\SQLEXPRESS";
-
-            //DB名
-            builder.InitialCatalog = "TestDB";
-
-            //認証方式(Windowsn認証)
-            builder.IntegratedSecurity = true;
-
-            _connectionString = builder.ToString();
+            
         }
 
         public static DataTable GetDataTable()
@@ -36,21 +21,7 @@ namespace testDB.SqlServer
 
             var sql = @"select * from Product";
 
-            DataTable dt = new DataTable();
-            //SQLサーバーに接続できる
-            //usingで書くことで接続を自動的に切ることができる
-            using (var connection = new SqlConnection(_connectionString))
-            //sql実行することができるのがadapter
-            using (var adapter = new SqlDataAdapter(sql, connection))
-            {
-                //接続
-                connection.Open();
-
-                //adapter実行する
-                //全部取る
-                adapter.Fill(dt);
-            }
-            return dt;
+            return SqlServerHelper.GetDataTable(sql);
         }
 
         public static List<ProductEntity> GetDataReader()
@@ -58,26 +29,15 @@ namespace testDB.SqlServer
             var sql = @"select * from Product";
 
             var result = new List<ProductEntity>();
-            using (var connection = new SqlConnection(_connectionString))
-            using (var command = new SqlCommand(sql, connection))
-            {
-                connection.Open();
-                //select分を一分一分返してくれる
-                using (var reader = command.ExecuteReader())
+
+            SqlServerHelper.Query(sql,
+                reader =>
                 {
-                    //読み込んでいるときはTrue
-                    while (reader.Read())
-                    {
-                        //DATAを取り出すときはObject型になる
-                        var productID = Convert.ToInt32(reader["ProductId"]);
-                        var productName = Convert.ToString(reader["ProductName"]);
-                        result.Add(new ProductEntity(
-                            Convert.ToInt32(reader["ProductId"]),
-                            Convert.ToString(reader["ProductName"]),
-                            Convert.ToInt32(reader["Price"])));
-                    }
-                }
-            }
+                    result.Add(new ProductEntity(
+                     Convert.ToInt32(reader["ProductId"]),
+                     Convert.ToString(reader["ProductName"]),
+                     Convert.ToInt32(reader["Price"])));
+                });
             return result;
         }
         public static void Insert(ProductEntity product)
@@ -89,16 +49,11 @@ ProductName,
 Price)
 values(@ProductId,@ProductName,@Price)";
 
-            using (var connection = new SqlConnection(_connectionString))
-            using (var command = new SqlCommand(sql, connection))
-            {
-                connection.Open();
-                command.Parameters.AddWithValue("@ProductId", product.ProductId);
-                command.Parameters.AddWithValue("@ProductName", product.ProductName);
-                command.Parameters.AddWithValue("@Price", product.Price);
-
-                command.ExecuteNonQuery();
-            }
+            var p = new List<SqlParameter>();
+            p.Add(new SqlParameter("@ProductId", product.ProductId));
+            p.Add(new SqlParameter("@ProductName", product.ProductName));
+            p.Add(new SqlParameter("@Price", product.Price));
+            SqlServerHelper.Execute(sql, p);
         }
 
         public static void Update(ProductEntity product)
@@ -110,19 +65,15 @@ set ProductName = @ProductName,
 price=@Price
 where ProductId = @ProductId";
 
-            using (var connection = new SqlConnection(_connectionString))
-            using (var command = new SqlCommand(sql, connection))
-            {
-                connection.Open();
-                command.Parameters.AddWithValue("@ProductId", product.ProductId);
-                command.Parameters.AddWithValue("@ProductName", product.ProductName);
-                command.Parameters.AddWithValue("@Price", product.Price);
+            var p = new List<SqlParameter>();
+            p.Add(new SqlParameter("@ProductId", product.ProductId));
+            p.Add(new SqlParameter("@ProductName", product.ProductName));
+            p.Add(new SqlParameter("@Price", product.Price));
+            var updatecount = SqlServerHelper.Execute(sql, p);
 
-                var updateCount = command.ExecuteNonQuery();
-                if (updateCount < 1)
-                {
-                    Insert(product);
-                }
+            if(updatecount < 1)
+            {
+                Insert(product);
             }
         }
 
@@ -133,15 +84,9 @@ where ProductId = @ProductId";
 delete Product
 where ProductId = @ProductId
 ";
-
-            using (var connection = new SqlConnection(_connectionString))
-            using (var command = new SqlCommand(sql, connection))
-            {
-                connection.Open();
-                command.Parameters.AddWithValue("@ProductId", productId);
- 
-                var updateCount = command.ExecuteNonQuery();
-            }
+            var p = new List<SqlParameter>();
+            p.Add(new SqlParameter("@ProductId", productId));
+            SqlServerHelper.Execute(sql, p);
         }
 
         //Nugetのdapperの使い方
@@ -149,7 +94,7 @@ where ProductId = @ProductId
         {
             var sql = @"select * from Product";
 
-            using (var connection = new SqlConnection(_connectionString))
+            using (var connection = new SqlConnection(SqlServerHelper.ConnectionString))
             {
                return connection.Query<ProductEntity>(sql).ToList();
             }
@@ -164,7 +109,7 @@ ProductName,
 Price)
 values(@ProductId,@ProductName,@Price)";
 
-            using (var connection = new SqlConnection(_connectionString))
+            using (var connection = new SqlConnection(SqlServerHelper.ConnectionString))
             {
                 connection.Execute(sql,
                     new
---
+++ SQL/testDB/testDB/SqlServer/SqlServerHelper.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDB.SqlServer
+{
+    public static class SqlServerHelper
+    {
+        internal static string ConnectionString { get; }
+
+        static SqlServerHelper()
+        {
+            //SQL接続にはDB名とテーブル名が必要である
+            //DB接続するためのコネクション
+            var builder = new SqlConnectionStringBuilder();
+
+            //サーバ名
+            //\があるとエラーになるため＠をつける
+            builder.DataSource = @"LAPTOP-CTHD7A6U\SQLEXPRESS";
+
+            //DB名
+            builder.InitialCatalog = "TestDB";
+
+            //認証方式(Windowsn認証)
+            builder.IntegratedSecurity = true;
+
+            ConnectionString = builder.ToString();
+        }
+
+        public static DataTable GetDataTable(string sql)
+        {
+
+            DataTable dt = new DataTable();
+            //SQLサーバーに接続できる
+            //usingで書くことで接続を自動的に切ることができる
+            using (var connection = new SqlConnection(ConnectionString))
+            //sql実行することができるのがadapter
+            using (var adapter = new SqlDataAdapter(sql, connection))
+            {
+                //接続
+                connection.Open();
+
+                //adapter実行する
+                //全部取る
+                adapter.Fill(dt);
+            }
+            return dt;
+        }
+
+        public static void Query(string sql,Action<SqlDataReader> action)
+        {
+            
+
+            var result = new List<ProductEntity>();
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                //select分を一分一分返してくれる
+                using (var reader = command.ExecuteReader())
+                {
+                    //読み込んでいるときはTrue
+                    while (reader.Read())
+                    {
+                        action(reader);
+                    }
+                }
+            }
+        }
+
+        public static int Execute(string sql,List<SqlParameter> sqlParametes)
+        {
+            //改行するときは@がいる
+
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                if(sqlParametes != null)
+                {
+                    command.Parameters.AddRange(sqlParametes.ToArray());
+                }
+
+                return command.ExecuteNonQuery();
+            }
+        }
+    }
+}
